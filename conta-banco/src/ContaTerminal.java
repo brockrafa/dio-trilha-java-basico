fae1ec05be6850535135a48325f9e8c035b1e343
rafaelsuporte99@hotmail.com <rafaelsuporte99@hotmail.com>
2023-08-28 18:28:09 -0300

    desenvolvimento do desafio

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o numero da conta:");
+        numero = sc.nextInt();
+        clearBuffer(sc);
+        System.out.println("Digite o numero da agencia:");
+        agencia = sc.nextLine();
+        System.out.println("Digite seu nome");
+        nomeCliente = sc.nextLine();
+        System.out.println("Digite o saldo atual da conta:");
+        saldo = sc.nextDouble();
+
+        System.out.println("Ola "+ nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", conta "+ numero +" e seu saldo "+ saldo +" já está disponível para saque");
+    }
+    private static void clearBuffer(Scanner scanner) {
+        if (scanner.hasNextLine()) {
+            scanner.nextLine();
+        }
     }
 }
